a1b4caedd1c77e3195a1354aa977e99c1bbcb422
elsayedmansour1 <70556762+elsayedmansour1@users.noreply.github.com>
2021-01-07 12:59:18 +0200

    Add files via upload

---
+++ include/DMA_Config.h
@@ -0,0 +1,28 @@
+#ifndef  _DMA_CONFIG_H
+#define  _DMA_CONFIG_H
+/****************************************************/
+	/**Peripheral Size**/
+/*Peripheral_Size_8
+  Peripheral_Size_16
+  Peripheral_Size_32*/
+
+#define Peripheral_Size Peripheral_Size_32
+/***************************************************/
+	/**Memory Size**/
+/*Memory_Size_8
+  Memory_Size_16
+  Memory_Size_32*/
+
+#define Memory_Size Memory_Size_32
+/***************************************************/
+/**priority level**/
+/* Low	
+   Medium	 
+   High	 
+   Very high */
+
+#define priority_level Very_high
+/***************************************************/
+
+
+#endif
